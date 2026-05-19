0af3ffaf0a7ed746c95a668817911fcfd656e309
wyattgill <wyattgill01@outlook.com>
2026-05-18 17:16:56 -0700

    resource-monitor: nixfmt the new fileset expression
    `fs.intersection (fs.gitTracked ./.) (fs.unions [ ... ])` from the
    prior commit was just shy of nixfmt's wrapping threshold and got
    flagged once `unions [...]` ran past it. Run `nixfmt` so the two-arg
    shape lays out with the unions list inside its own parens.

--- modules/services/resource-monitor/default.nix
+++ modules/services/resource-monitor/default.nix
@@ -18,15 +18,17 @@ let
 
   siteSrc = fs.toSource {
     root = ./site;
-    fileset = fs.intersection (fs.gitTracked ./.) (fs.unions [
-      ./site/index.html
-      ./site/package.json
-      ./site/package-lock.json
-      ./site/eslint.config.js
-      ./site/tsconfig.json
-      ./site/src
-      ./site/vite.config.js
-    ]);
+    fileset = fs.intersection (fs.gitTracked ./.) (
+      fs.unions [
+        ./site/index.html
+        ./site/package.json
+        ./site/package-lock.json
+        ./site/eslint.config.js
+        ./site/tsconfig.json
+        ./site/src
+        ./site/vite.config.js
+      ]
+    );
   };
 
   vmConfig = {
