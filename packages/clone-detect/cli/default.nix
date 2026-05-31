dbd68ace34e2711e9b9883ae1eaeb483a04585fa
Andrew Gazelka <andrew.gazelka@gmail.com>
2026-05-30 18:28:30 -0700

    clone-detect: expose clone and ast-merge flake packages

---
+++ packages/clone-detect/cli/default.nix
@@ -0,0 +1,11 @@
+{ ix, lib, ... }:
+
+ix.cargoUnit.selectBinaryWithTests ix.rustWorkspace.units {
+  binary = "clone";
+  packageName = "clone-cli";
+  meta = {
+    description = "Code clone and duplication detector (Type-1/2/3) over tree-sitter ASTs";
+    license = lib.licenses.mit;
+    mainProgram = "clone";
+  };
+}
