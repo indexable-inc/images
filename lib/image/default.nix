2a3032da59855bd30ca407364c7f4e93b9a6d4bb
Andrew Gazelka <andrew.gazelka@gmail.com>
2026-07-23 02:05:42 -0700

    style: alejandra

--- lib/image/default.nix
+++ lib/image/default.nix
@@ -239,7 +239,6 @@
 
   mkVm = mkVmFor system;
 
-
   # Dev-fleet layer over `mkFleet` (RFC 0007): consumes the forkable `ix.nix`
   # spec. Curried like `mkFleetFor` so example/flake eval can target a host
   # system.
